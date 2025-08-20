fe88a957c1dbb21c57c50c228fd27296c93a8545
Berkay <berkay@Berkays-MacBook-Air.local>
2025-08-20 12:08:37 +0300

    first init

---
+++ EterationBerkayKayaCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EterationBerkayKayaCase
+//
+//  Created by Berkay on 20.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
